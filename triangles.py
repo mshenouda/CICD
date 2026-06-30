df8bea1133505eb427273bc20f40d952a8ae6330
mshenouda <34300034+mshenouda@users.noreply.github.com>
2026-06-29 21:49:54 -0700

    Update triangles.py

--- triangles.py
+++ triangles.py
@@ -6,10 +6,9 @@ b = float(input("Enter side b: "))
 c = float(input("Enter side c: "))
 
 # Calculate the semi-perimeter
-s = (a + b + c) / 2
+s = (a + b + c) /2
 
-# Calculate the area
+#Calculate the area
 area = math.sqrt(s * (s - a) * (s - b) * (s - c))
-
 # Display the result
 print(f"The area of the triangle is: {area:.2f}")
